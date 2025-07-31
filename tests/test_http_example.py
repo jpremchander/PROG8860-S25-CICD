446057fe190a1d1ef97f45cf6eb6f622c88e3393
jpremchander <premchander.j.pc@gmail.com>
2025-07-31 19:29:54 -0400

    Add configs

---
+++ tests/test_http_example.py
@@ -0,0 +1,11 @@
+import HttpExample.__init__ as function
+
+class MockRequest:
+    def __init__(self):
+        pass
+
+def test_main():
+    req = MockRequest()
+    res = function.main(req)
+    assert res.status_code == 200
+    assert res.get_body().decode() == "Hello, world!"
